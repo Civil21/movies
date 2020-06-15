d833ab63c8255ba7b2e2246cb45ce094390f8c15
Civil21 <diusolexander@gmail.com>
2020-06-15 20:59:28 +0300

    show category

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'movies#index'
   resources :movies, only: %w[show index]
+  resources :categories, only: %w[show index]
 
   get :search, to: 'movies#search'
 end
